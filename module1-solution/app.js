0b6cd872aaba05429982b3e2bdda0bbd67ad6e4b
Joel Martinez <joel.esli@gmail.com>
2016-09-28 23:46:00 +0200

    Experimenting with writing a function in javascript

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,3 +1,6 @@
+
+String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
+
 (function () {
 'use strict';
 
@@ -19,16 +22,44 @@ LunchCheckController.$inject = ['$scope'];
     }
     else {
 
-      var array = $scope.lunch.split(",")
-      if (array.length <= 3) {
-        $scope.message = "Enjoy!";
+      var myLunch = $scope.lunch.replace(/ /g, "");
+      // var myLunch = replaceAll($scope.lunch, " ", "")
+      console.log("Replace 1 " + myLunch);
+      while (myLunch.contains(" ")) {
+        myLunch = myLunch.replace(" " , "");
+      }
+
+      // myLunch = replaceAll(myLunch, ",," , ",")
+      while (myLunch.contains(",,")) {
+        myLunch = myLunch.replace(",," , ",");
+      }
+      console.log("Replace 2 " + myLunch);
+      var array = myLunch.split(",");
+
+      if (myLunch == "," || myLunch == "" ) {
+          $scope.message = "Please enter data first!" + "\t" + array.length + "\t" + myLunch;
+      }
+      else if (array.length <= 3) {
+        $scope.message = "Enjoy!" + "\t" + array.length + "\t" + myLunch;
       }
       else {
-        $scope.message = "Too much!";
+        $scope.message = "Too much! "+ "\t" + array.length + "\t" + myLunch;
       }
 
     }
   };
+
+  function replaceAll(string, oldString, newString) {
+
+    console.log(string);
+    while (string.indexOf(oldString) >= 0) {
+      string = string.replace(/oldString/g, newString);
+      console.log(string);
+    }
+
+    return string;
+  };
+
 };
 
 })();
